ff5729f2994274e526810822e6a3414f8867e317
Alex <NikolausC@users.noreply.github.com>
2018-05-23 13:50:32 +0100

    Merge devel into WIP (#35)
    * Add support for WIP environment colours (#28)
    
    * Add support for WIP environment colours
    
    * Update gems
    
    * Update navbar gem
    
    * UAT2 styling
    
    * Add missing curly brace
    
    * New line
    
    * Don't use gradients for dashboard panel headers
    
    * Renaming (#34)
    
    * Submission -> Reception. Work Orders -> Work Plans. Description updates.
    
    * Update navbar gem
    
    * Fix tests
    
    * Update test environment config

--- spec/views/dashboard/index.html.erb_spec.rb
+++ spec/views/dashboard/index.html.erb_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe "dashboard/index.html.erb", type: :view do
     it "renders application summary partials" do
       render
       expect(response).to render_template :index
-      expect(response).to render_template(partial: '_submission')
+      expect(response).to render_template(partial: '_reception')
       expect(response).to render_template(partial: '_sets')
       expect(response).to render_template(partial: '_permissions')
       expect(response).to render_template(partial: '_projects')
-      expect(response).to render_template(partial: '_work_orders')
+      expect(response).to render_template(partial: '_work')
     end
   end
 end
